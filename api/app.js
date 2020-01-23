138a8604a2bc4272911b8faced41822595b61183
mjmccull0 <mjmccull@uncg.edu>
2020-01-23 14:06:43 -0500

    Added nodemon to api/package.json and revert a change to api/app.js

--- api/app.js
+++ api/app.js
@@ -42,9 +42,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-// Runs the Node application locally.
-app.listen(3000, function() {
-	console.log('Server started on localhost:3000');
-});
-
 module.exports = app;
