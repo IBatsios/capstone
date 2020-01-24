5973217cb989c4db992df7e1044291e196b654f9
mjmccull0 <mjmccull@uncg.edu>
2020-01-24 14:05:24 -0500

    Experimenting with React Router and creating components.  React Router allows for switch of the content on the page while also updating the url and browser history, which enables users to use browser back and forward buttons.  The components created thus far are simplistic; but as they become more complex the purpose for using index.js in each component will become clear.

---
+++ client/src/components/Watercooler/Watercooler.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Watercooler extends Component {
+  render() {
+    return (
+      <h2>Watercooler</h2>
+    );
+  }
+}
+
+export default Watercooler;
