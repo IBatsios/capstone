633aea2f3db4b662dcc4803ff06bef592a5524d0
mjmccull0 <mjmccull@uncg.edu>
2020-01-30 12:55:46 -0500

    Added material-ui node_modules, so update your packages.  Started implementing tabs component, it will need to be reworked to fetch data from the server at some point.

--- client/src/App.js
+++ client/src/App.js
@@ -6,46 +6,30 @@ import {
   Link,
   NavLink
 } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 import { Home } from './components/Home';
 import { About } from './components/About';
 import { Watercooler } from './components/Watercooler';
 import logo from './logo.svg';
 import './App.css';
+import SimpleTabs from './components/Tabs/SimpleTabs';  
 
 class App extends Component {
+  state = {
+    tabs: [
+      { label: "Home", content: <Home />},
+      { label: "Watercooler", content: <Watercooler />},
+      { label: "About Us", content: <About />}
+    ]
+  };
+
   render() {
     return (
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <NavLink to="/">Home</NavLink>
-              </li>
-              <li>
-                <NavLink to="/watercooler">Watercooler</NavLink>
-              </li>
-              <li>
-                <NavLink to="/about">About</NavLink>
-              </li>
-            </ul>
-          </nav>
-
-          {/* A <Switch> looks through its children <Route>s and
-              renders the first one that matches the current URL. */}
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/watercooler">
-              <Watercooler />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+      <React.Fragment>
+        <CssBaseline />
+        <SimpleTabs tabs={this.state.tabs} />
+      </React.Fragment>
     );
   }
 }
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import SimpleTabs from '../Tabs/SimpleTabs';  
 
 class Home extends Component {
+  state = {
+    tabs: [
+      { label: "Item One", content: "Item One content"},
+      { label: "Item Two", content: "Item Two content"},
+      { label: "Item Three", content: "Item Three content"}
+    ]
+  };
+
   render() {
     return (
-      <h2>Home</h2>
+      <React.Fragment>
+        <h2>Home</h2>
+        <SimpleTabs tabs={this.state.tabs} />
+      </React.Fragment>
     );
   }
 }
