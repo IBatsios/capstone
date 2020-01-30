0d9988c0115f438221549d838a4270f44f608745
Christopher <csteven015@gmail.com>
2020-01-30 14:35:30 -0500

    Added Comment schema.

--- api/models/comment.js
+++ api/models/comment.js
@@ -1,1 +1,12 @@
-// SCHEMA HERE.
+const mongoose = require("mongoose");
+
+var commentSchema = new mongoose.Schema({
+    content: String,
+    authorId: String,
+    dateCreated: Date,
+    dateEdited: Date,
+    isActive: Boolean // In lieu of permanently deleting the Comment.
+});
+
+// Allows the Comment Schema to be used outside of this file.
+module.exports = mongoose.model("Comment", commentSchema);
