ccf6dd30e45437a65c320375f2443ab851edfae8
Christopher <csteven015@gmail.com>
2020-01-30 13:42:50 -0500

    Added authorId field for Post schema.

--- api/models/posts.js
+++ api/models/posts.js
@@ -3,10 +3,11 @@ const mongoose = require("mongoose");
 var postSchema = new mongoose.Schema({
     title: String,
     content: String,
+    authorId: String,
     datePosted: Date,
     dateEdited: Date,
     isActive: Boolean
 });
 
 // Allows the User Schema to be used outside of this file.
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("Post", postSchema);
