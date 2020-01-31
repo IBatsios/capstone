6677c01f63dcc16b2abc2569e0b27f46750c59ea
Christopher <csteven015@gmail.com>
2020-01-30 20:29:31 -0500

    Removed authorId from comment.js.

--- api/models/comment.js
+++ api/models/comment.js
@@ -9,7 +9,6 @@ const mongoose = require("mongoose");
 
 var commentSchema = new mongoose.Schema({
     content: String,
-    // authorId: String, // userSchema should probably handle this functionality.
     dateCreated: Date,
     dateEdited: Date,
     isActive: Boolean // In lieu of permanently deleting the Comment.
