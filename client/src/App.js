cec62d49c2523352c6afbdafdc6c5243984170d6
mjmccull0 <mjmccull@uncg.edu>
2020-02-03 10:35:04 -0500

    Removed currently unused imports.

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,9 @@
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  NavLink
-} from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import { Home } from './components/Home';
 import { About } from './components/About';
 import { Watercooler } from './components/Watercooler';
-import logo from './logo.svg';
 import './App.css';
 import SiteTabs from './components/Tabs/SiteTabs';  
 
