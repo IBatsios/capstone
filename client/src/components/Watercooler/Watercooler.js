38e4e6eead58653e46d7a241fecaa07f9dc4026d
mjmccull0 <mjmccull@uncg.edu>
2020-02-03 15:47:50 -0500

    Added a set of tabs to Watercooler, and implemented a way for active tabs on each page (Home and Watercooler) to persist until the browser session is over.

--- client/src/components/Watercooler/Watercooler.js
+++ client/src/components/Watercooler/Watercooler.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
+import SimpleTabs from '../Tabs/SimpleTabs';  
 
 class Watercooler extends Component {
+  constructor(props) {
+    super(props);
+    this.displayName = 'Watercooler';
+
+    this.state = {
+      tabs: [
+        { label: "Watercooler Item One", content: "Watercooler Item One content"},
+        { label: "Watercooler Item Two", content: "Watercooler Item Two content"},
+        { label: "Watercooler Item Three", content: "Watercooler Item Three content"}
+      ],
+      activeTab: parseInt(sessionStorage.getItem(this.displayName)) || 0 
+    };
+  }
+
+  handleValueChange = (newValue) => {
+    this.setState({ activeTab: newValue }); 
+    this.forceUpdate();
+  }
+
+  componentWillUnmount() {
+    sessionStorage.setItem(
+      this.displayName,
+      this.state.activeTab.toString()
+    );
+  }
+
   render() {
     return (
-      <h2>Watercooler</h2>
+      <React.Fragment>
+        <h2>Watercooler</h2>
+        <SimpleTabs
+          tabs={this.state.tabs}
+          value={this.state.activeTab}
+          onValueChange={this.handleValueChange} />
+      </React.Fragment>
     );
   }
 }
