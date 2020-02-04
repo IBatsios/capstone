c689d8eda8dcbd221fb202fff28512746e4595ac
mjmccull0 <mjmccull@uncg.edu>
2020-02-03 19:15:03 -0500

    Attempting to further refactor example code and remove needless complications

--- client/src/components/Tabs/SiteTabs.js
+++ client/src/components/Tabs/SiteTabs.js
@@ -1,41 +1,9 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <Typography
-      component="div"
-      role="tabpanel"
-      hidden={value !== index}
-      id={`site-tabpanel-${index}`}
-      aria-labelledby={`site-tab-${index}`}
-      {...other}
-    >
-      {value === index && <Box p={3}>{children}</Box>}
-    </Typography>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-};
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
-  },
-}));
+import TabPanel from './TabPanel';
+import TabsStyles from './TabsStyles';
 
 
 class SiteTabs extends Component {
@@ -47,21 +15,14 @@ class SiteTabs extends Component {
     this.setState({ value: newValue});
   };
 
-  a11yProps = (index) => {
-    return {
-      id: `site-tab-${index}`,
-      'aria-controls': `site-tabpanel-${index}`,
-    };
-  }
-
   render() {
-    const  classes = useStyles; 
+    const  classes = TabsStyles; 
     return (
       <div className={classes.root}>
         <AppBar position="static">
-          <Tabs value={this.state.value} onChange={this.handleChange} aria-label="site tabs">
+          <Tabs value={this.state.value} onChange={this.handleChange} centered>
             {this.props.tabs.map((tab, index) => (
-              <Tab label={tab.label} {...this.a11yProps(index)} key={index} />
+              <Tab label={tab.label} key={index} />
             ))} 
           </Tabs>
         </AppBar>
---
+++ client/src/components/Tabs/TabsStyles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const TabsStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default TabsStyles;
