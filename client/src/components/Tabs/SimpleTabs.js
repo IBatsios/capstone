f38699cc3feeb301aa91313dcc9f89b636d9f601
mjmccull0 <mjmccull@uncg.edu>
2020-02-03 19:15:03 -0500

    Attempting to further refactor example code and remove needless complications

--- client/src/components/Tabs/SimpleTabs.js
+++ client/src/components/Tabs/SimpleTabs.js
@@ -1,42 +1,9 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <Typography
-      component="div"
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && <Box p={3}>{children}</Box>}
-    </Typography>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-};
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
-  },
-}));
-
+import TabPanel from './TabPanel';
+import TabsStyles from './TabsStyles';
 
 class SimpleTabs extends Component {
 
@@ -44,21 +11,14 @@ class SimpleTabs extends Component {
     this.props.onValueChange(value);
   };
 
-  a11yProps = (index) => {
-    return {
-      id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
-    };
-  }
-
   render() {
-    const  classes = useStyles;
+    const  classes = TabsStyles;
     return (
       <div className={classes.root}>
         <AppBar position="static">
           <Tabs value={this.props.value} onChange={this.handleChange} aria-label="simple tabs">
             {this.props.tabs.map((tab, index) => (
-              <Tab label={tab.label} {...this.a11yProps(index)} key={index} />
+              <Tab label={tab.label} key={index} />
             ))} 
           </Tabs>
         </AppBar>
---
+++ client/src/components/Tabs/TabPanel.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      {...other}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </Typography>
+  );
+}
+
+
+export default TabPanel;
