bcae54aa016be56ca7d75810833c802f4ce719b1
Christopher Thacker <csteven015@gmail.com>
2020-02-24 22:08:11 -0500

    Added DatabaseConnector.

--- api/connectors/database.connector.js
+++ api/connectors/database.connector.js
@@ -1,3 +1,25 @@
+'use strict' // Chrome support for ES6.
+
+const translatorName = 'mongoose.translator';
+
+const Translator = require('../translators/' + translatorName);
+
 /**
- * Database Connector
+ * Database Connector: all calls to the database will be funneled through this file.
+ *
+ * @author Christopher Thacker
+ * @since 1.0.0
  */
+class DatabaseConnector {
+    constructor(uri, translator) {
+        this.uri = uri;
+        this.translator = translator;
+    }
+
+    connect() {
+        const translator = new Translator(this.uri);
+        translator.connect();
+    }
+}
+
+module.exports = DatabaseConnector;
