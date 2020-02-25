17893d2ddcad20ba1841aaff67738787f790016b
Christopher Thacker <csteven015@gmail.com>
2020-02-24 22:08:49 -0500

    Added MongoTranslator.

--- api/translators/mongoose.translator.js
+++ api/translators/mongoose.translator.js
@@ -1,3 +1,24 @@
+'use strict' // Chrome support for ES6.
+
+const mongoose = require('mongoose');
+
 /**
- * MongoDB Translator
+ * MongoDB Translator: translator class for MongoDB.
+ * 
+ * @author Christopher Thacker
+ * @since 1.0.0
  */
+class MongoTranslator {
+    constructor(uri) {
+        this.uri = uri;
+    }
+
+    connect() {
+        console.log("Connecting to MongoDB...");
+        mongoose.connect(this.uri, {useNewUrlParser: true, useUnifiedTopology: true})
+            .then(() => console.log("MongoDB connected!"))
+            .catch(error => console.log(error));
+    }
+}
+
+module.exports = MongoTranslator;
