ef32800060fc64f82cfab65e1bc2a60399b6a4f2
Christopher Thacker <csteven015@gmail.com>
2020-02-27 13:21:38 -0500

    Added skeleton CRUD methods to the connector and translator for the DB.

--- api/translators/MongoTranslator.js
+++ api/translators/MongoTranslator.js
@@ -9,21 +9,54 @@ const mongoose = require('mongoose');
  * @since 1.0.0
  */
 class MongoTranslator {
+    /**
+     * Constructor method for MongoDB translator class.
+     * 
+     * @param {string} uri
+     * @author Christopher Thacker
+     * @since 1.0.0
+     */
     constructor(uri) {
         this.uri = uri;
     }
 
+    /**
+     * Connect method for MongoDB translator class; connects to a MongoDB database
+     * that is passed into the translator upon creating its instance.
+     * 
+     * @author Christopher Thacker
+     * @since 1.0.0
+     */
     connect() {
         console.log("Connecting to MongoDB...");
         mongoose.connect(this.uri, {useNewUrlParser: true, useUnifiedTopology: true})
             .then(() => console.log("MongoDB connected!"))
-            .catch(error => {
-                console.log("Failed to establish connection with MongoDB.")
-                // console.log(error) // Logs entire stack trace; write to a log file in the future.
-            });
+            .catch(error => console.log(`MongoDB connection error: ${error.message}`));
     }
 
-    // TODO: Add CRUD operations by utilizing Mongo's built in functions...
+    close() {
+        // TODO: method to close the database connection when it's necessary.
+    }
+    
+    create(obj) {
+        console.log("Inside MongoDB Translator CREATE");
+        // TODO: perform create operation in DB.
+    }
+
+    read(obj) {
+        console.log("Inside MongoDB Translator READ");
+        // TODO: perform get operation in DB.
+    }
+
+    update(obj) {
+        console.log("Inside MongoDB Translator UPDATE");
+        // TODO: perform update operation in DB.
+    }
+
+    delete(obj) {
+        console.log("Inside MongoDB Translator DELETE");
+        // TODO: perform delete operation in DB.
+    }
 }
 
 module.exports = MongoTranslator;
