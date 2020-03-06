7f3885a32c4c154a91a0f981c4518971157b95a2
mjmccull0 <mjmccull@uncg.edu>
2020-03-06 10:48:14 -0500

    Removed some styling code from layout/Interest.js. Refactored layout/Interest.js to use a newly created css module for styling.  These changes create a layout for interests which have independently scrollable columns for sidebars and content, assuming the user is using a large enough screen and there is enough content in each section.  On smaller screens the sidebars are hidden. This resolves #51.

--- client/src/layout/Interest.js
+++ client/src/layout/Interest.js
@@ -1,42 +1,28 @@
 import React from "react";
-import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
+import Hidden from '@material-ui/core/Hidden';
+import classes from './Interest.module.css';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-}));
 
 const Interest = (props) => {
+
   const { sidebar, content, ads } = props;
 
-  const classes = useStyles();
-
   return (
-      <Grid container spacing={3}>
-        <Grid item xs>
-          <Paper className={classes.paper}>
-            {sidebar}
-          </Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>
-            {content}
-          </Paper>
-        </Grid>
-        <Grid item xs>
-          <Paper className={classes.paper}>
-            {ads}
-          </Paper>
-        </Grid>
-    </Grid>
+    <div className={classes.interests}>
+      <Hidden smDown>
+        <div className={classes.sidebar}>
+          <div className={classes.wrapper}>{sidebar}</div>
+        </div>
+      </Hidden>
+      <div className={classes.content}>
+        <div className={classes.wrapper}>{content}</div>
+      </div>
+      <Hidden smDown>
+        <div className={classes.ads}>
+          <div className={classes.wrapper}>{ads}</div>
+        </div>
+      </Hidden>
+    </div>
   );
 }
 
