466aa9b2f03e56f1545e925ef6f8dd3c9f187760
Christopher Thacker <csteven015@gmail.com>
2020-03-07 12:57:27 -0500

    Fleshed out some user routes. TODO: put and delete

--- api/routes/users.route.js
+++ api/routes/users.route.js
@@ -1,3 +1,5 @@
+'use strict'
+
 /**
  * This is the routes file for all actions related to users.
  * 
@@ -8,37 +10,77 @@
 const router = require('express').Router();
 const UserServices = require('../services/UserServices');
 
-// INDEX: show all users.
+/**
+ * INDEX: show all users.
+ * 
+ * @author Christopher Thacker
+ * @since 1.0.0
+ */
 router.get('/', async (req, res) => {
-    const filter = req.body;
+    const filter = req.body; // Optional TODO: Outsource to a UserServices function to build filter.
 
-    const allUsers = await UserServices.getAllUsers(filter);
+    var allUsers = await UserServices.getManyUsers(filter);
+
+    if (!allUsers) {
+        return res.send('No users found.');
+    }
+
     return res.json(allUsers);
 });
 
-// CREATE: add a new user.
-router.post('/', (req, res) => {
-    res.send("This will eventually add a new user to the database!");
+/**
+ * CREATE: add a new user.
+ * 
+ * @author Christopher Thacker
+ * @since 1.0.0
+ */
+router.post('/', async (req, res) => {
+    const userDTO = req.body; // Optional TODO: Outsource to a UserServices function to build DTO.
+
+    var userId = await UserServices.addUser(userDTO);
+    return res.json(userId);
 });
 
-// NEW: renders the form to register a new user.
+/**
+ * NEW: renders the form to register a new user.
+ * 
+ * @author Christopher Thacker
+ * @since 1.0.0
+ */
 router.get('/new', (req, res) => {
-    res.send("This will eventually render the form for creating a new user!");
+    return res.render('users/new');
 });
 
-// SHOW: displays user page for an existing user.
-router.get('/:id', (req, res) => {
-    res.send("This will eventually show more detailed information about a single user!");
+/**
+ * SHOW: displays user page for an existing user.
+ * 
+ * @author Christopher Thacker
+ * @since 1.0.0
+ */
+router.get('/:id', async (req, res) => {
+    const userId = req.params.id;
+    var foundUser = await UserServices.getUser(userId);
+    if (!foundUser) {
+        console.log('Error when retrieving user.');
+        return res.redirect('/users');
+    }
+    // console.log(foundUser);
+    return res.render('uses/show', {user: foundUser});
 });
 
-// EDIT: renders the form to edit an existing user.
+/**
+ * EDIT: renders the form to edit an existing user.
+ * 
+ * @author Christopher Thacker
+ * @since 1.0.0
+ */
 router.get('/:id/edit', (req, res) => {
-    res.send("This will eventually render the form to edit a user!");
+    return res.render('users/edit', {user: req.user});
 });
 
 // PUT: updates a user in the database.
 router.put('/:id', (req, res) => {
-    res.send("This will eventually update a user inside the database!");
+    const userId = req.params.id;
 });
 
 // DELETE: deactivates an existing user in the database (NOT permanent deletion).
