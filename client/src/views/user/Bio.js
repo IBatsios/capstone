d9f882095dccf132fde64fdc469b6e919eccd25c
mjmccull0 <mjmccull@uncg.edu>
2020-03-20 16:07:06 -0400

    Added different avatars for the mock users.

--- client/src/views/user/Bio.js
+++ client/src/views/user/Bio.js
@@ -1,15 +1,19 @@
 import React, { useContext } from 'react';
+import Avatar from '@material-ui/core/Avatar';
 import { UserContext } from 'data/UserStore';
 import classes from './Bio.module.css';
 export const Bio = () => {
   const [state, dispatch] = useContext(UserContext);
 
   // TODO: Move this default image to a config file.
-  const imgUrl = state.user.avatar || '/avatar.png';
   return (
     <div className={classes.wrapper}>
       <div className={classes.sidebar}>
-        <img alt="avatar" src={imgUrl} />
+        <Avatar
+          className={classes.avatar}
+          alt={state.user.username}
+          src={state.user.avatar}
+        />
       </div>
       <div className={classes.content}>
         <div>Username: {state.user.username}</div>
