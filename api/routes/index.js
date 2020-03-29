6bd164bbec612276c623d348c1bea47fd7f9ef2d
Christopher Thacker <csteven015@gmail.com>
2020-03-28 21:09:28 -0400

    Added skeleton register routes to index route.

--- api/routes/index.js
+++ api/routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Featurama' });
 });
 
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+router.post('/register', function(req, res, next) {
+  res.send('Signing up...');
+});
+
 module.exports = router;
