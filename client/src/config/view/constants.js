c15ed4fadddf47686cf0a0f60cc2796bd1de2223
mjmccull0 <mjmccull@uncg.edu>
2020-04-01 10:13:41 -0400

    Added a new config file to hold view constants.  The file currently contains only the constants used in the add UserMenu.  The user menu currently only lists options, nothing happens when clicked.  The options are Add Post, Profile Settings (edit the bio here), and Log out.

---
+++ client/src/config/view/constants.js
@@ -0,0 +1,3 @@
+export const ADD_POST = "Add Post";
+export const PROFILE_SETTINGS = "Profile Settings";
+export const LOG_OUT = "Log out";
---
+++ client/src/views/user/UserMenu.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import Drawer from '@material-ui/core/Drawer';
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import PostAddIcon from '@material-ui/icons/PostAdd';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import SettingsIcon from '@material-ui/icons/Settings';
+import {
+  ADD_POST,
+  PROFILE_SETTINGS,
+  LOG_OUT
+} from 'config/view/constants';
+
+export const UserMenu = (props) => {
+  const [state, setState] = React.useState({
+    menu: false
+  });
+
+  // The direction the menu enters from.
+  const menuAnchor = 'left';
+
+  const toggleDrawer = (open) => event => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, 'menu': open });
+  };
+
+  const userMenuOptions = () => (
+    <div
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        <ListItem button>
+          <ListItemIcon>
+            <PostAddIcon />
+          </ListItemIcon>
+          <ListItemText primary={ADD_POST} />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <SettingsIcon />
+          </ListItemIcon>
+          <ListItemText primary={PROFILE_SETTINGS} />
+        </ListItem>
+      </List>
+      <Divider />
+      <List>
+        <ListItem button>
+          <ExitToAppIcon />
+          <ListItemText primary={LOG_OUT} />
+        </ListItem>
+      </List>
+    </div>
+  );
+
+  return (
+    <div>
+      <IconButton
+        edge={props.edge}
+        onClick={toggleDrawer(true)}
+        aria-label="open drawer">
+        <MenuIcon />
+      </IconButton>
+      <Drawer
+        anchor={menuAnchor}
+        open={state.menu}
+        onClose={toggleDrawer(false)}
+       >
+        {userMenuOptions()}
+      </Drawer>
+    </div>
+  );
+}
