bc30c60cbbefe5dcb77823e065e61e1a98b74698
jwthrs <jeweathe@uncg.edu>
2020-04-03 23:48:49 -0400

    fixed small syntax mistakes causing api server to crash

--- api/routes/posts.route.js
+++ api/routes/posts.route.js
@@ -1,6 +1,6 @@
 /**
  * This is the routes file for all actions related to posts.
- * 
+ *
  * @author Christopher Thacker
  * @author Jamie Weathers
  * @since 1.0.0
@@ -10,91 +10,90 @@ const router = require('express').Router()
 const PostServices = require('../services/PostServices')
 
 // INDEX: show all posts.
-router.get('/', (req, res) => {
-    const filter = req.body;
+router.get('/', async (req, res) => {
+  const filter = req.body
 
-    const allPosts = await PostServices.getMany(filter);
+  const allPosts = await PostServices.getMany(filter)
 
-    if (!allPosts) {
-        return res.redirect('/posts/new');
-    }
+  if (!allPosts) {
+    return res.redirect('/posts/new')
+  }
 
-  res.render('posts', {posts : allPosts});
+  res.render('posts', { posts: allPosts })
 })
 
 // CREATE: add a new post.
-router.post('/', (req, res) => {
-  const postDTO = req.body;
-  const result = await PostServices.addNew(postDTO);
+router.post('/', async (req, res) => {
+  const postDTO = req.body
+  const result = await PostServices.addNew(postDTO)
 
-  var response;
+  var response
   if (!result) {
-    response = 'Post was unsuccessful.';
+    response = 'Post was unsuccessful.'
   } else {
-      response = 'Post successful.';
+    response = 'Post successful.'
   }
 
-  res.send(response);
+  res.send(response)
 })
 
 // NEW: renders the form to add a new post.
-router.get('/new', (req, res) => {
-    res.render('posts/');
+router.get('/new', async (req, res) => {
+  res.render('posts/')
 })
 
 // SHOW: displays more information about an existing post.
-router.get('/:id', (req, res) => {
-    const postId = req.params.id;
-    const postResult = PostServices.getById(postId);
+router.get('/:id', async (req, res) => {
+  const postId = req.params.id
+  const postResult = PostServices.getById(postId)
 
-    if (!postResult) {
-        console.log('Error attempting to get post.');
-        return res.redirect('/posts');
-    }
+  if (!postResult) {
+    console.log('Error attempting to get post.')
+    return res.redirect('/posts')
+  }
 
-    return res.send('posts/show', {post: postResult});
+  return res.send('posts/show', { post: postResult })
 })
 
 // EDIT: renders the form to edit an existing post.
-router.get('/:id/edit', (req, res) => {
-    const postId = req.params.id;
-    const postResult = await PostServices.getById(postId);
+router.get('/:id/edit', async (req, res) => {
+  const postId = req.params.id
+  const postResult = await PostServices.getById(postId)
 
-    if (!postResult) {
-        console.log('Error when attempting to render edit post form.');
-        return res.render('/posts');
-    }
+  if (!postResult) {
+    console.log('Error when attempting to render edit post form.')
+    return res.render('/posts')
+  }
 
-    return res.render('posts/edit',{post: postResult})
+  return res.render('posts/edit', { post: postResult })
 })
 
 // PUT: updates a post in the database.
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+  const newPostData = req.body
+  const postId = req.params.id
+  const updatedPost = await PostServices.update(postId, newPostData)
 
-    const newPostData = req.body;
-    const postId = req.params.id;
-    const updatedPost = await PostServices.update(postId, newPostData);
+  if (!updatedPost) {
+    console.log('Error when updating post.')
+    return res.redirect('/posts')
+  }
 
-    if (!updatedPost) {
-        console.log('Error when updating post.');
-        return res.redirect('/posts');
-    }
-
-   return res.redirect(`posts/${postId}`);
+  return res.redirect(`posts/${postId}`)
 })
 
 // DELETE: turns off a certain post within the database (NOT permanent deletion).
-router.delete('/:id', (req, res) => {
-    // Set is active to false.
+router.delete('/:id', async (req, res) => {
+  // Set is active to false.
 
-    const postId = req.params.id;
-    const hiddenPost = await PostServices.hide(postId);
+  const postId = req.params.id
+  const hiddenPost = await PostServices.hide(postId)
 
-    if (!hiddenPost) {
-        console.log('Error when deleting post.');
-        return res.redirect('/posts');
-    }
-    return res.send('Post hidden.');
+  if (!hiddenPost) {
+    console.log('Error when deleting post.')
+    return res.redirect('/posts')
+  }
+  return res.send('Post hidden.')
 })
 
-module.exports = router;
+module.exports = router
--- api/services/PostServices.js
+++ api/services/PostServices.js
@@ -8,148 +8,138 @@ const Post = require(`../models/${modelName}`)
 /**
  * Post Services class
  * Contains functions to retrieve or modify information in the database.
- * 
+ *
  * @author Jamie Weathers
  * @since 1.0.0
  */
 
 class PostServices {
-
   /**
    * Adds a new post to the database.
-   * 
+   *
    * @param {Object} postDTO Post data transfer object.
    * @returns {Object|false} Post object or false if creating a new post unsuccessful
    * @author Jamie Weathers
    * @since 1.0.0
    */
-    static async addNew(postDTO) {
+  static async addNew(postDTO) {
+    try {
+      const newPost = new Post({
+        title: postDTO.title,
+        content: postDTO.content,
+        topic: postDTO.topic,
+        likeCount: postDTO.likeCount,
+        arrayLike: postDTO.arrayLike,
+        comments: postDTO.comments,
+        author: postDTO.author,
+        isActive: postDTO.isActive
+      })
 
-        try {
+      const result = await connector.create(modelName, newPost)
+      if (!result) {
+        console.log('New post failed at PostServices')
+        return false
+      }
+      return result
+    } catch (error) {
+      console.log(error)
+      return false
+    }
+  }
 
-            const newPost = new Post({
-                title = postDTO.title,
-                content = postDTO.content,
-                topic = postDTO.topic,
-                likeCount = postDTO.likeCount,
-                arrayLike = postDTO.arrayLike,
-                comments = postDTO.comments,
-                author = postDTO.author,
-                isActive = postDTO.isActive
-                });
+  /**
+   *
+   * @param {ObjectId|string} postId Associated post reference identification to the intended post document.
+   * @returns {Object|null|false} Returns the post Object if it is found, null if it is not found, and false if unsuccessful.
+   * @author Jamie Weathers
+   * @since 1.0.0
+   */
+  static async getById(postId) {
+    const postResult = await connector.readOne(modelName, postId)
 
-            const result = await connector.create(modelName, newPost);
-            if (!result) {
-                console.log('New post failed at PostServices');
-                 return false;
-             }
-            return result;
-
-        } catch (error) {
-            console.log(error);
-            return false;
-        }
-
+    if (postResult === false) {
+      console.log(`Error: bad post ID [${postId}].`)
     }
 
-    /**
-     * 
-     * @param {ObjectId|string} postId Associated post reference identification to the intended post document.
-     * @returns {Object|null|false} Returns the post Object if it is found, null if it is not found, and false if unsuccessful.
-     * @author Jamie Weathers
-     * @since 1.0.0
-     */
-    static async getById(postId) {
-        const postResult = await connector.readOne(modelName, postId);
-
-        if (postResult === false) {
-            console.log(`Error: bad post ID [${postId}].`);
-        }
-
-        if (postResult === null) {
-            console.log(`Error: post with ID [${postId} not found.]`)
-        }
-
-        return postResult;
+    if (postResult === null) {
+      console.log(`Error: post with ID [${postId} not found.]`)
     }
 
-    /**
-     * Gets an array of posts based on the provided filter conditions.
-     * If not filter defined, all posts will be returned.
-     * @param {Object} filter 
-     * @returns {Object|null|false} the post object if successful | null if no post found | false if unsuccessful
-     * @author Jamie Weathers
-     * @since 1.0.0
-     */
-    static async getMany(filter) {
+    return postResult
+  }
 
-        //TODO: Need a filter utility.
+  /**
+   * Gets an array of posts based on the provided filter conditions.
+   * If not filter defined, all posts will be returned.
+   * @param {Object} filter
+   * @returns {Object|null|false} the post object if successful | null if no post found | false if unsuccessful
+   * @author Jamie Weathers
+   * @since 1.0.0
+   */
+  static async getMany(filter) {
+    //TODO: Need a filter utility.
 
-        const allPosts = await connector.readMany(modelName, filter);
+    const allPosts = await connector.readMany(modelName, filter)
 
-        if (!allPosts) {
-            console.log('Could not find any posts with the provided query.');
-        }
-
-        return allPosts;
+    if (!allPosts) {
+      console.log('Could not find any posts with the provided query.')
     }
 
-    /**
-     * Updates a post given new data.
-     * 
-     * @param {ObjectId|string} postId Associated post reference identification to the intended post document.
-     * @param {Object} newData Dictionary containing the intended field changes.
-     * @returns {Object|null|false} Returns the updated post object if successful, null if the post does not exist, and false if unsuccessful.
-     * @author Jamie Weathers
-     * @since 1.0.0
-     */
-    static async update(postId, newData) {
+    return allPosts
+  }
 
-        const updatedPost = await connector.update(modelName, postId, newData);
+  /**
+   * Updates a post given new data.
+   *
+   * @param {ObjectId|string} postId Associated post reference identification to the intended post document.
+   * @param {Object} newData Dictionary containing the intended field changes.
+   * @returns {Object|null|false} Returns the updated post object if successful, null if the post does not exist, and false if unsuccessful.
+   * @author Jamie Weathers
+   * @since 1.0.0
+   */
+  static async update(postId, newData) {
+    const updatedPost = await connector.update(modelName, postId, newData)
 
-        if (updatedPost === null) {
-            console.log(`Could not find post ID [${postId}] to update.`);
-        }
-
-        if (updatedPost === false) {
-            console.log(`Update post ID [${postId}] failed.`)
-        }
-
-        return updatedPost;
+    if (updatedPost === null) {
+      console.log(`Could not find post ID [${postId}] to update.`)
     }
 
-    /**
-     * Changes the 'isActive' field of the post to false.
-     * 
-     * @param {ObjectId|string} postId Associated post reference identification to the intended post document.
-     * @returns {boolean} true if succcessful, false if post is not found or already hidden.
-     * @author Jamie Weathers
-     * @since 1.0.0
-     */
-    static async hide(postId) {
-
-        const hideData = {'isActive' : 'false'};
-        const getPost = await this.update(postId, hideData);
-
-        return getPost;
+    if (updatedPost === false) {
+      console.log(`Update post ID [${postId}] failed.`)
     }
 
-    /**
-     * Changes the 'isActive' field of the post to true.
-     * 
-     * @param {ObjectIdstring} postId
-     * @returns {boolean} true if successful, false if post not found or already showing.
-     * @author Jamie Weathers
-     * @since 1.0.0
-     */
-    static async show(postId) {
-        const hideData = {'isActive' : 'true'};
-        const getPost = await this.update(postId, hideData);
+    return updatedPost
+  }
 
-        return getPost;
-    }
+  /**
+   * Changes the 'isActive' field of the post to false.
+   *
+   * @param {ObjectId|string} postId Associated post reference identification to the intended post document.
+   * @returns {boolean} true if succcessful, false if post is not found or already hidden.
+   * @author Jamie Weathers
+   * @since 1.0.0
+   */
+  static async hide(postId) {
+    const hideData = { isActive: 'false' }
+    const getPost = await this.update(postId, hideData)
 
+    return getPost
+  }
+
+  /**
+   * Changes the 'isActive' field of the post to true.
+   *
+   * @param {ObjectId|string} postId Associated post reference identification to the intended post document.
+   * @returns {boolean} true if successful, false if post not found or already showing.
+   * @author Jamie Weathers
+   * @since 1.0.0
+   */
+  static async show(postId) {
+    const hideData = { isActive: 'true' }
+    const getPost = await this.update(postId, hideData)
+
+    return getPost
+  }
 }
 
-
-module.exports = PostServices;
+module.exports = PostServices
