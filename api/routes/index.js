638b085d01dfa5800bb693040672e6faf8fe35a3
Christopher Thacker <csteven015@gmail.com>
2020-04-07 21:26:41 -0400

    Added logic to have register POST route contact bcrypt to encrypt the user's password before sending it. Added commented-out status returns for when the front-end is connected.

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const UserServices = require('../services/UserServices');
 const User = require('../models/user.model');
 const Middleware = require('../utility/Middleware');
+const Validation = require('../utility/Validation');
 
 /**
  * Home Page for back-end developer testing.
@@ -32,19 +36,61 @@ router.get('/register', function (req, res, next) {
  * @since 1.0.0
  */
 router.post('/register', async function (req, res, next) {
-    const newUser = req.body;
 
-    try {
-        const result = await UserServices.addUser(newUser);
-        if (result) {
-            return res.redirect('/');
-        }
-        console.log('Add user failed');
-        return res.render('register');
-    } catch (error) {
-        console.log(error);
-        return res.render('register');
+    // Form validation
+    const { errors, isValid } = Validation.validateRegisterInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+        return res.status(400).json(errors);
     }
+
+    const foundUser = await UserServices.getManyUsers({ email: req.body.email });
+
+    if (foundUser) {
+        return res.status(400).json({ email: 'Email already exists' });
+    } else {
+        try {
+
+            // Encrypt password before sending to database
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(req.body.password, salt, async (err, hash) => {
+                    if (err) throw err;
+                    req.body.password = hash;
+                    
+                    // Add user to the database.
+                    const newUser = await UserServices.addUser(req.body);
+
+                    if (newUser) {
+                        // return res.status(200).json(user);   // Uncomment when front-end is connected.
+                        return res.redirect('/');               // Remove when front-end is connected.
+                    } else {
+                        // return res.status(500); 
+                        console.log('Error adding user to DB');
+                        return res.render('register');
+                    }
+                });
+            });
+        } catch (error) {
+            // errors.exception = error.message;
+            console.log(error);
+            res.render('register');
+        }
+    }
+
+    // const newUser = req.body;
+
+    // try {
+    //     const result = await UserServices.addUser(newUser);
+    //     if (result) {
+    //         return res.redirect('/');
+    //     }
+    //     console.log('Add user failed');
+    //     return res.render('register');
+    // } catch (error) {
+    //     console.log(error);
+    //     return res.render('register');
+    // }
 });
 
 /**
@@ -68,7 +114,7 @@ router.post('/login', passport.authenticate('local',
         successRedirect: '/',
         failureRedirect: '/login'
     }), function (req, res, next) {
-});
+    });
 
 /**
  * Logs the user out using PassportJS.
