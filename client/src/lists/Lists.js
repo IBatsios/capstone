32d7c9ee2013eb4496e73013376f3ef91cc4fb59
mjmccull0 <mjmccull@uncg.edu>
2020-04-07 21:06:59 -0400

    Modifying sections of the site to user different stores.

--- client/src/lists/Lists.js
+++ client/src/lists/Lists.js
@@ -2,13 +2,15 @@ import React, { useContext, Fragment } from "react";
 import { UserContext } from 'data/UserStore';
 import { TabsUi } from 'views/tabs/Tabs';
 import { LISTS } from 'config/user';
+import { ListStore } from 'data/ListStore';
+import { ListContext } from 'data/ListStore';
 
 const Lists = () => {
-  const [state, dispatch] = useContext(UserContext);
-
   return (
     <Fragment>
-      <TabsUi section={ LISTS } />
+      <ListStore>
+        <TabsUi context={ListContext} section={ LISTS } />
+      </ListStore>
     </Fragment>
   );
 }
