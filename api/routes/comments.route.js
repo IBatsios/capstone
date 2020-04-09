bc1552d5d3a9c709b8582a0778cbce595d58d755
jwthrs <jeweathe@uncg.edu>
2020-04-09 02:10:25 -0400

    update & edit functionalities

--- api/routes/comments.route.js
+++ api/routes/comments.route.js
@@ -41,26 +41,55 @@ router.get('/new', (req, res) => {
 
 // SHOW: displays more information about an existing comment.
 router.get('/:id', (req, res) => {
-  res.send(
-    'This will eventually show more detailed information about a single comment!'
-  )
+    const commentId = req.params.id
+    const commentResult = await commentServices.getById(commentId)
+  
+    if (!commentResult) {
+      console.log('Error attempting to get comment.')
+      return res.redirect('/comments')
+    }
+  
+    return res.render('comments/showcomment', { comment: commentResult })
 })
 
 // EDIT: renders the form to edit an existing comment.
 router.get('/:id/edit', (req, res) => {
-  res.send('This will eventually render the form to edit a comment!')
+    const commentId = req.params.id
+    const commentResult = await commentServices.getById(commentId)
+  
+    if (!commentResult) {
+      console.log('Error when attempting to render edit comment form.')
+      return res.render('/comments')
+    }
+  
+    return res.render('comments/editcomment', { comment: commentResult })
 })
 
 // PUT: updates a post in the database.
 router.put('/:id', (req, res) => {
-  res.send('This will eventually update a comment inside the database!')
+    const newCommentData = req.body
+    const commentId = req.params.id
+    const updatedComment = await CommentServices.update(commentId, newCommentData)
+  
+    if (!updatedComment) {
+      console.log('Error when updating comment.')
+      return res.redirect('/comments')
+    }
+  
+    return res.redirect(`comments/${commentId}`)
 })
 
 // DELETE: turns off a certain comment within the database (NOT permanent deletion).
 router.delete('/:id', (req, res) => {
-  res.send(
-    "This will eventually turn off a single comment so it isn't displayed in the application! \nNOTE: DO NOT USE A PERMANENT DELETE METHOD."
-  )
+
+  const commentId = req.params.id
+  const hiddenComment = await CommentServices.hide(commentId)
+
+  if (!hiddenComment) {
+    console.log('Error when deleting comment.')
+    return res.redirect('/comments')
+  }
+  return res.send('Comment hidden.')
 })
 
 module.exports = router
