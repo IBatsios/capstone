c3211c22e9d05791e5f70fce6b5e2c085e45d12c
jwthrs <jeweathe@uncg.edu>
2020-04-09 01:59:25 -0400

    comment route skeleton

---
+++ api/routes/comments.route.js
@@ -0,0 +1,50 @@
+/**
+ * This is the routes file for all actions related to comments.
+ *
+ * @author Jamie Weathers
+ * @since 1.0.0
+ */
+
+const router = require('express').Router()
+let Post = require('../models/comment.model')
+
+// INDEX: show all posts.
+router.get('/', (req, res) => {
+  res.send('This will eventually show ALL comments by users!')
+})
+
+// CREATE: add a new post.
+router.post('/', (req, res) => {
+  res.send('This will eventually add a new comment to the database!')
+})
+
+// NEW: renders the form to add a new post.
+router.get('/new', (req, res) => {
+  res.send('This will eventually render the form for creating a new comment!')
+})
+
+// SHOW: displays more information about an existing post.
+router.get('/:id', (req, res) => {
+  res.send(
+    'This will eventually show more detailed information about a single comment!'
+  )
+})
+
+// EDIT: renders the form to edit an existing post.
+router.get('/:id/edit', (req, res) => {
+  res.send('This will eventually render the form to edit a comment!')
+})
+
+// PUT: updates a post in the database.
+router.put('/:id', (req, res) => {
+  res.send('This will eventually update a comment inside the database!')
+})
+
+// DELETE: turns off a certain post within the database (NOT permanent deletion).
+router.delete('/:id', (req, res) => {
+  res.send(
+    "This will eventually turn off a single comment so it isn't displayed in the application! \nNOTE: DO NOT USE A PERMANENT DELETE METHOD."
+  )
+})
+
+module.exports = router
