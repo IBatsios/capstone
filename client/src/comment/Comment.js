8f7ed97c9c6f1a65c901e25e12ea67865af05275
mjmccull0 <mjmccull@uncg.edu>
2020-04-11 13:41:40 -0400

    Moved the logic into comments/ContextActions.

--- client/src/comment/Comment.js
+++ client/src/comment/Comment.js
@@ -1,72 +1,16 @@
-import React, { useContext } from "react";
-import { UserContext } from 'data/UserStore';
+import React from "react";
 import Avatar from '@material-ui/core/Avatar';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Hidden from '@material-ui/core/Hidden';
-import { ContextActions } from 'ContextActions';
+import { ContextActions } from 'comment/ContextActions';
 import classes from './Comment.module.css';
 import { CommentForm } from 'comment/CommentForm';
 
 
 export const Comment = (props) => {
-  const [state, dispatch] = useContext(UserContext);
-
-  const handleDelete = (comment) => {
-    dispatch({
-      store: 'PostStore',
-      type: 'deleteComment',
-      payload: comment
-    });
-  };
-
-
-  const handleEditComment = (comment) => {
-    const { id, content } = { ...comment};
-    const commentData = { id, content };
-    dispatch({
-      type: 'pushBlock',
-      payload: <CommentForm {...commentData} /> 
-    });
-  };
-
-  const handleLike = (commentId) => {
-    dispatch({
-      store: 'PostStore',
-      type: 'likeComment',
-      payload: commentId 
-    });
-  };
-
-  const handleDislike = (commentId) => {
-    dispatch({
-      store: 'PostStore',
-      type: 'dislikeComment',
-      payload: commentId 
-    });
-  };
-
-  const handleFriendRequest = (author) => {
-    dispatch({
-      type: 'newFriendRequest',
-      payload: {
-        userId: state.user.id,
-        friendId: author.id
-      }
-    });
-  };
-
-  const handleReport = (commentId) => {
-    dispatch({
-      store: 'PostStore',
-      type: 'reportComment',
-      payload: commentId 
-    });
-  }
-
-
   return (
     <>
       <Card className={classes.root}>
@@ -79,16 +23,7 @@ export const Comment = (props) => {
           }
           action={
             <ContextActions
-              userId={state.user.id}
-              id={props.comment.id}
-              comment={props.comment}
-              author={props.comment.author}
-              onEditComment={(comment) => handleEditComment(comment)}
-              onDelete={(comment) => handleDelete(comment)}
-              onLike={(commentId) => handleLike(commentId)}
-              onDislike={(commentId) => handleDislike(commentId)}
-              onFriendRequest={(author) => handleFriendRequest(author)}
-              onReport={(commentId) => handleReport(commentId)}
+              {...props.comment}
             />
           }
           title={
