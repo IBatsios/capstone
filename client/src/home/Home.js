61835f1632213bfd1b0b75422c5c97966d30345f
mjmccull0 <mjmccull@uncg.edu>
2020-04-13 16:45:38 -0400

    Home.js, Watercooler.js, and Lists.js now contain the logic which controls what appears in there section respectively.  Previously some of this logic was in Interest and TabsUi; though this reduced duplication of code—it made it hard to reason about and brittle.  This commit adds nearly identical code to Home.js, Watercooler.js, and Lists.js—this makes the code easier to think about, allows for easier customization in the future and is more resilient given our current goals.  As a little more background on why the code is so similiar, I was trying to refact the duplicate code into another file; but wasn't able to due to mental strain and time restrictions.

--- client/src/home/Home.js
+++ client/src/home/Home.js
@@ -1,10 +1,48 @@
 import React, { useContext } from "react";
-import { TabsUi } from 'tabs/Tabs';
-import { HOME } from 'config/user';
+import Tab from "@material-ui/core/Tab";
+import { UserContext } from 'data/UserStore';
+import { Ads, ContentHeader, Sidebar } from 'layout/Layout';
+import { Tabs } from "interest/Tabs";
+import TabPanel from 'tabs/TabPanel';
+import Posts from 'posts/Posts';
+import 'interest/interest.css';
 
 const Home = () => {
+  const [state] = useContext(UserContext);
+  const section = 'home';
+
+  const tabs = state.interests.map((interest, index) => (
+    <Tab label={interest} value={index} key={index} />
+  ));
+
+  const tabPanels = state.interests.map((interest, index) => (
+    <TabPanel value={state.section[section].interest} index={index} key={index}>
+      <div className="interests">
+        {Sidebar({section, interest})}
+        <div className="content">
+          <div className="wrapper">
+            {ContentHeader({section, interest})}
+            <Posts posts={
+              state.posts.filter(post => {
+                return post.interest === interest && post.author.id === state.user.id
+              })
+            } />
+          </div>
+        </div>
+        {Ads({section, interest})}
+      </div>
+    </TabPanel>
+  ));
+
   return (
-      <TabsUi section={ HOME } />
+    <>
+      <Tabs
+        section={section}
+        interests={state.interests}
+        tabs={tabs}
+        tabPanels={tabPanels}
+      />
+    </>
   );
 }
 
--- client/src/lists/Lists.js
+++ client/src/lists/Lists.js
@@ -1,10 +1,48 @@
-import React, { Fragment } from "react";
-import { TabsUi } from 'tabs/Tabs';
-import { LISTS } from 'config/user';
+import React, { useContext } from "react";
+import Tab from "@material-ui/core/Tab";
+import { UserContext } from 'data/UserStore';
+import { Ads, ContentHeader, Sidebar } from 'layout/Layout';
+import { Tabs } from "interest/Tabs";
+import TabPanel from 'tabs/TabPanel';
+import { Listing } from 'lists/Listing';
+import 'interest/interest.css';
 
 const Lists = () => {
+  const [state] = useContext(UserContext);
+  const section = 'lists';
+
+  const tabs = state.interests.map((interest, index) => (
+    <Tab label={interest} value={index} key={index} />
+  ));
+
+  const tabPanels = state.interests.map((interest, index) => (
+    <TabPanel value={state.section[section].interest} index={index} key={index}>
+      <div className="interests">
+        {Sidebar({section, interest})}
+        <div className="content">
+          <div className="wrapper">
+            {ContentHeader({section, interest})}
+            <Listing lists={
+              state.lists.filter(list => {
+                return list.interest === interest && list.author.id === state.user.id
+              })
+            } />
+          </div>
+        </div>
+        {Ads({section, interest})}
+      </div>
+    </TabPanel>
+  ));
+
   return (
-    <TabsUi section={ LISTS } />
+    <>
+      <Tabs
+        section={section}
+        interests={state.interests}
+        tabs={tabs}
+        tabPanels={tabPanels}
+      />
+    </>
   );
 }
 
