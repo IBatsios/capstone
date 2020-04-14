9f02987e485b562f9092b835792548cdd2a0362f
mjmccull0 <mjmccull@uncg.edu>
2020-04-14 14:53:05 -0400

    Added Copyright component to the App component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 import User from 'user';
+import { Copyright } from 'layout/Layout';
 
 class App extends Component {
   render() {
-    return <User />;
+    return (
+      <>
+        <User />
+        <Copyright className="copyright" />
+      </>
+    );
   }
 }
 
