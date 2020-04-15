a8fbe9b8f72002c7995fda112cc0b267323d5754
mjmccull0 <mjmccull@uncg.edu>
2020-04-14 21:22:27 -0400

    src/user/User.js now monitors whether a visitor is logged-in or not and will show the user's content or login and registration forms.

--- client/src/user/User.js
+++ client/src/user/User.js
@@ -1,4 +1,4 @@
-import React, { useContext, Fragment } from "react";
+import React, { useContext } from "react";
 import {
   AppBar,
   BottomNavigation,
@@ -23,6 +23,8 @@ import Watercooler from "watercooler/Watercooler";
 import TabPanel from 'tabs/TabPanel';
 import { UserContext } from 'data/UserStore';
 import { UserMenu } from './UserMenu';
+import { Login } from 'login/login';
+import { Register } from 'register/register';
 
 /**
  * The user-interface for an authenticated user. 
@@ -42,60 +44,69 @@ const User = () => {
   }
 
   return (
-     <Fragment>
-        <Toolbar>
-          <AppBar position="fixed" color="default">
+    <>
+      {state.authenticated
+        ? <>
             <Toolbar>
-              <UserMenu edge="start" />
+              <AppBar position="fixed" color="default">
+                <Toolbar>
+                  <UserMenu edge="start" />
+                </Toolbar>
+                <Hidden xsDown>
+                  <Tabs className={classes.tabs} value={active} centered onChange={onTabChange}>
+                    <Tab label={HOME} {...a11yProps(0)} />
+                    <Tab label={WATERCOOLER} {...a11yProps(1)} />
+                    <Tab label={LISTS} {...a11yProps(2)} />
+                  </Tabs>
+                </Hidden> 
+              </AppBar>
             </Toolbar>
-            <Hidden xsDown>
-              <Tabs className={classes.tabs} value={active} centered onChange={onTabChange}>
-                <Tab label={HOME} {...a11yProps(0)} />
-                <Tab label={WATERCOOLER} {...a11yProps(1)} />
-                <Tab label={LISTS} {...a11yProps(2)} />
-              </Tabs>
-            </Hidden> 
-          </AppBar>
-       </Toolbar>
-       <TabPanel value={active} index={0} {...otherProps}>
-         <Home />
-       </TabPanel>
-       <TabPanel value={active} index={1} {...otherProps}>
-         <Watercooler />
-       </TabPanel>
-       <TabPanel value={active} index={2} {...otherProps}>
-         <Lists />
-       </TabPanel>
+            <TabPanel value={active} index={0} {...otherProps}>
+              <Home />
+            </TabPanel>
+            <TabPanel value={active} index={1} {...otherProps}>
+              <Watercooler />
+            </TabPanel>
+            <TabPanel value={active} index={2} {...otherProps}>
+              <Lists />
+            </TabPanel>
 
-      <Hidden smUp>
-        <BottomNavigation
-           value={active}
-           onChange={onTabChange}
-           showLabels
-           className={classes.root}
-        >
-
-          <BottomNavigationAction
-            label={HOME}
-            icon={<HOME_ICON />}
-            classes={{label: classes.label}}
-          />
-          <BottomNavigationAction
-            label={WATERCOOLER}
-            icon={<WATERCOOLER_ICON />}
-            classes={{label: classes.label}}
-          />
-          <BottomNavigationAction
-            label={LISTS}
-            icon={<LISTS_ICON />}
-            classes={{label: classes.label}}
-          />
-        </BottomNavigation>
-      </Hidden>
-      {state.dynamicContent &&
-        state.dynamicContent[0]
-      }
-    </Fragment>
+            <Hidden smUp>
+              <BottomNavigation
+                 value={active}
+                 onChange={onTabChange}
+                 showLabels
+                 className={classes.root}
+              >
+                <BottomNavigationAction
+                  label={HOME}
+                  icon={<HOME_ICON />}
+                  classes={{label: classes.label}}
+                />
+                <BottomNavigationAction
+                  label={WATERCOOLER}
+                  icon={<WATERCOOLER_ICON />}
+                  classes={{label: classes.label}}
+                />
+                <BottomNavigationAction
+                  label={LISTS}
+                  icon={<LISTS_ICON />}
+                  classes={{label: classes.label}}
+                />
+              </BottomNavigation>
+            </Hidden>
+            {state.dynamicContent &&
+              state.dynamicContent[0]
+            }
+          </>
+        : <>
+           { state.login
+             ? <Login />
+             : <Register />
+           }
+          </>
+       }
+     </>
   );
 }
 
