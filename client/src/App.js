fbd055b8b748574862385ec10db0c48b617fde10
mjmccull0 <mjmccull@uncg.edu>
2020-04-14 21:21:28 -0400

    Returned App.js to a more sensible and functional state.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,12 @@
 import React from "react";
-import { useAuth } from 'data/NoAuthStore';
-import { NoAuthStore } from 'data/NoAuthStore';
-import { NoAuth } from 'user/NoAuth';
 import User from 'user';
 import { Copyright } from 'layout/Layout';
 
 function App() {
-  const [state, dispatch] = useAuth();
-
   return (
     <>
-      {state.authenticated
-       ? <> 
-           <User />
-           <Copyright className="copyright" />
-         </>
-       : <>
-           <NoAuthStore>
-             <NoAuth />
-           </NoAuthStore>
-         </>
-      }
+     <User />
+     <Copyright className="copyright" />
     </>
   );
 }
