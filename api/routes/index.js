ef1f5687f2bd1c0b11dd061f5fad20b069063b93
Christopher Thacker <csteven015@gmail.com>
2020-04-15 23:35:47 -0400

    Readded basic LocalStrategy for PassportJS. This will be refactored and abstracted out to fit our architecture better, but for now it works.

--- api/routes/index.js
+++ api/routes/index.js
@@ -37,47 +37,27 @@ router.get('/register', function (req, res, next) {
  * @since 1.0.0
  */
 router.post('/register', async function (req, res, next) {
-
-    // Form validation
-    const { errors, isValid } = Validation.validateRegisterInput(req.body);
-
-    // Check validation
-    if (!isValid) {
-        return res.status(400).json(errors);
-    }
-
-    const foundUser = await UserServices.getUserByEmail(req.body.email);
-
-    if (foundUser) {
-        return res.status(400).json({ email: 'Email already exists' });
-    } else {
-        try {
-
-            // Encrypt password before sending to database
-            bcrypt.genSalt(10, (err, salt) => {
-                bcrypt.hash(req.body.password, salt, async (err, hash) => {
-                    if (err) throw err;
-                    req.body.password = hash;
-                    
-                    // Add user to the database.
-                    const newUser = await UserServices.addUser(req.body);
-
-                    if (newUser) {
-                        // return res.status(200).json(user);   // Uncomment when front-end is connected.
-                        return res.redirect('/');               // Remove when front-end is connected.
-                    } else {
-                        // return res.status(500); 
-                        console.log('Error adding user to DB');
-                        return res.render('register');
-                    }
-                });
-            });
-        } catch (error) {
-            // errors.exception = error.message;
-            console.log(error);
-            res.render('register');
+    const newUser = new User({
+        email: req.body.email,
+        password: req.body.password,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        username: req.body.username,
+        avatar: req.body.avatar,
+        bio: req.body.bio,
+        phone: req.body.phone,
+        isActive: true
+    });
+    User.register(newUser, req.body.password, function (err, user) { // TODO: abstract this out.
+        if (err) {
+            console.log(err);
+            return false;
         }
-    }
+        passport.authenticate('local')(req, res, function () {
+            // res.redirect('/');
+            res.status(200).json(req.user);
+        });
+    });
 });
 
 /**
@@ -91,66 +71,18 @@ router.get('/login', function (req, res, next) {
 });
 
 /**
- * Authenticates the user using PassportJS and JWTs.
+ * Authenticates the user using PassportJS.
  * 
  * @author Christopher Thacker
  * @since 1.0.0
  */
-router.post('/login', async function(req, res, next) {
-    // Form validation
-    const {errors, isValid} = Validation.validateLoginInput(req.body);
-
-    // Check validation
-    if (!isValid) {
-        return res.status(400).json(errors);
-    }
-
-    const email = req.body.email;
-    const password = req.body.password;
-
-    const foundUser = await UserServices.getUserByEmail(email);
-
-    // Check if user exists
-    if (!foundUser) {
-        // return res.status(404).json({emailnotfound: 'Email not found'});
-        console.log('Email not found');
-        return res.render('login');
-    }
-
-    // Check password
-    bcrypt.compare(password, foundUser.password).then(isMatch => {
-        if (isMatch) {
-
-            // Create JWT Payload
-            const payload = {
-                id: foundUser.id,
-                username: foundUser.username
-            };
-
-            // Sign token
-            jwt.sign(
-                payload,
-                auth.secretOrKey,
-                {
-                    expiresIn: auth.tokenExp
-                },
-                (err, token) => {
-                    res.json({
-                        success: true,
-                        token: "Bearer " + token
-                    });
-                }
-            );
-        } else {
-            // return res.status(400).json({passwordincorrect: 'Password incorrect'});
-            console.log('Password incorrect');
-            return res.render('login');
-        }
-    }).catch(err => {
-        console.log(err.message);
-        return res.render('login');
+router.post('/login', passport.authenticate('local',
+    {
+        // successRedirect: '/',
+        // failureRedirect: '/login'
+    }), function (req, res) {
+        return res.status(200).json(req.user);
     });
-});
 
 /**
  * Logs the user out using PassportJS.
