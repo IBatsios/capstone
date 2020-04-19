bc34339e7e26056f779ed0733d8c5bc929da47ea
mjmccull0 <mjmccull@uncg.edu>
2020-04-19 14:55:40 -0400

    Removed unused import.

--- client/src/login/login.js
+++ client/src/login/login.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import axios from "axios";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
