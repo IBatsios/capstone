58d61a680927b5d65474bb9a0625d5b2a85552e0
mjmccull0 <mjmccull@uncg.edu>
2020-04-22 12:59:41 -0400

    Removed properties which weren't not intended to be apart of a list item and added a listId property.

--- api/models/item.model.js
+++ api/models/item.model.js
@@ -8,28 +8,26 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var itemSchema = new Schema({
-    itemName:{
+    name:{
         type: String,
         required: true
     },
-    URL:{
+    listId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'List',
+      required: true
+    },
+    name:{
         type: String,
         required: true
     },
-    interest: {
+    url:{
         type: String,
         required: true
     },
     description: {
         type: String,
         required: true
-    },
-    author: {
-        id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User'
-        },
-        username: String
     },
     isActive: {
         type: Boolean,
