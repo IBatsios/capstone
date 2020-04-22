6a08efc2336d082f097144591b83fb38da71dd38
jwthrs <jeweathe@uncg.edu>
2020-04-22 16:36:29 -0400

    Removed unnecessary author tags

--- api/routes/posts.route.js
+++ api/routes/posts.route.js
@@ -1,9 +1,6 @@
 /**
  * This is the routes file for all actions related to posts.
- * TODO: Create form views.
- * @author Christopher Thacker
- * @author Jamie Weathers
- * @author Michael McCulloch
+ * @author Jamie Weathers ref Christopher Thacker
  * @since 1.0.0
  */
 
@@ -26,19 +23,15 @@ router.get('/', async (req, res) => {
 // CREATE: add a new post.
 router.put('/', async (req, res) => {
   const postDTO = req.body
-  const user = req.session.user;
+  const user = req.session.user
   const result = await PostServices.addNew(user, postDTO)
 
   if (!result) {
     return res.status(404).send({ error: `Create Post unsuccessful` })
   }
 
-<<<<<<< HEAD
-  res.status(200).send(result)
-=======
   // Provides the url of the new post in the request response.
   res.status(200).redirect(`${result._id}`)
->>>>>>> 8a81f0d34da1fed0529709d989fcc39c7bc94315
 })
 
 // SHOW: displays more information about an existing post.
