327a18c3c934603f831ab66b9d86f80b8e5c11e0
mjmccull0 <mjmccull@uncg.edu>
2020-04-21 20:45:40 -0400

    Change the post add method to match the post update method.  Got a reference to the authenticated user to be used in the author portion of the post, and returned the url for the added post.

--- api/routes/posts.route.js
+++ api/routes/posts.route.js
@@ -3,6 +3,7 @@
  * TODO: Create form views.
  * @author Christopher Thacker
  * @author Jamie Weathers
+ * @author Michael McCulloch
  * @since 1.0.0
  */
 
@@ -23,15 +24,17 @@ router.get('/', async (req, res) => {
 })
 
 // CREATE: add a new post.
-router.post('/', async (req, res) => {
+router.put('/', async (req, res) => {
   const postDTO = req.body
-  const result = await PostServices.addNew(postDTO)
+  const user = req.session.user;
+  const result = await PostServices.addNew(user, postDTO)
 
   if (!result) {
     return res.status(404).send({ error: `Create Post unsuccessful` })
   }
 
-  res.status(200).send()
+  // Provides the url of the new post in the request response.
+  res.status(200).redirect(`${result._id}`)
 })
 
 // SHOW: displays more information about an existing post.
