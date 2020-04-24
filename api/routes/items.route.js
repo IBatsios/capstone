791b2a369e797238efcbb47c1725b94d95d119bd
jwthrs <jeweathe@uncg.edu>
2020-04-24 15:36:23 -0400

    Put and get items

--- api/routes/items.route.js
+++ api/routes/items.route.js
@@ -2,121 +2,131 @@
 
 /**
  * This is the routes file for all actions related to items.
- * 
+ *
  * @author Hieu Vo ref Christopher Thacker
  * @since 1.0.0
  */
 
-const router = require('express').Router();
-const ItemServices = require('../services/ItemServices');
+const router = require('express').Router()
+const ItemServices = require('../services/ItemServices')
 
 /**
  * INDEX: show all items.
- * 
+ *
  * @author Hieu Vo ref Christopher Thacker
  * @since 1.0.0
  */
 router.get('/', async (req, res) => {
-    // const filter = req.body; // Optional TODO: Outsource to a ItemServices function to build filter.
-    const filter = {};
+  // const filter = req.body; // Optional TODO: Outsource to a ItemServices function to build filter.
+  const filter = {}
 
-    var allItems = await ItemServices.getManyItems(filter);
+  var allItems = await ItemServices.getManyItems(filter)
 
-    if (!allItems) {
-        // return res.send('No items found.');
-        return res.redirect('/items/newItem');
-    }
+  if (!allItems) {
+    // return res.send('No items found.');
+    return res.redirect('/items/newItem')
+  }
 
-    return res.render('items', {items: allItems});
-});
+  return res.render('items', { items: allItems })
+})
 
 /**
  * CREATE: add a new Item.
- * 
+ *
  * @author Hieu Vo ref Christopher Thacker
+ * @author Jamie Weathers ref Christopher Thacker and Hieu Vo
  * @since 1.0.0
  */
 router.post('/', async (req, res) => {
-    req.body.isActive = true;
-    const itemDTO = req.body; // Optional TODO: Outsource to a temServices function to build DTO.
+  if (!req.session.user) {
+    return res.status(401).json({ error: 'User not logged in' })
+  }
 
-    var newItem = await ItemServices.addItem(itemDTO);
+  try {
+    const itemDTO = req.body
+
+    var newItem = await ItemServices.addItem(itemDTO)
 
     if (!newItem) {
-        return res.redirect('/items/newItem');
+      return res.status(400).send({ error: 'New Item Failed!' })
     }
 
-    return res.render('items/showItem', {item: newItem});
-});
-
-/**
- * NEW: renders the form to register a new Item.
- * 
- * @author Hieu Vo ref Christopher Thacker
- * @since 1.0.0
- */
-router.get('/newItem', (req, res) => {
-    return res.render('items/newItem');
-});
+    return res.status(200).send()
+  } catch (err) {
+    console.log(error.message)
+    return res.status(500).send(error)
+  }
+})
 
 /**
  * SHOW: displays item page for an existing item.
- * 
+ *
  * @author Hieu Vo ref Christopher Thacker
+ * @author Jamie Weathers ref Christopher Thacker and Hieu Vo
  * @since 1.0.0
  */
 router.get('/:id', async (req, res) => {
-    const itemId = req.params.id;
-    var foundItem = await ItemServices.getItem(itemId);
+  if (!req.session.user) {
+    return res.status(401).json({ error: 'User not logged in' })
+  }
+
+  try {
+    const itemId = req.params.id
+    var foundItem = await ItemServices.getItem(itemId)
     if (!foundItem) {
-        console.log('Error when retrieving item.');
-        return res.redirect('/items');
+      console.log('Error when retrieving item.')
+      return res.status(403).json({ error: 'Item could not be retrieved.' })
     }
-    return res.render('items/showItem', {item: foundItem});
-});
+
+    return res.status(200).send(foundItem)
+  } catch (err) {
+    console.log(error.message)
+    return res.status(500).send(error)
+  }
+})
 
 /**
  * EDIT: renders the form to edit an existing Item.
- * 
+ *
  * @author Hieu Vo ref Christopher Thacker
  * @since 1.0.0
  */
 router.get('/:id/edit', async (req, res) => {
-    const foundItem = await ItemServices.getItem(req.params.id);
-    return res.render('items/editItem', {item: foundItem});
-});
+  const foundItem = await ItemServices.getItem(req.params.id)
+  return res.render('items/editItem', { item: foundItem })
+})
 
 /**
  * PUT: updates a Item in the database.
- * 
+ *
  * @author Hieu Vo ref Christopher Thacker
  * @since 1.0.0
  */
 router.put('/:id', async (req, res) => {
-    const newData = req.body;
-    const itemId = req.params.id;
-    const updatedItem = await ItemServices.updateItem(itemId, newData);
-    if (!updatedItem) {
-        console.log('Error when updating item.');
-        return res.redirect('/items');
-    }
-    return res.redirect(`/items/${itemId}`);
-});
+  const newData = req.body
+  const itemId = req.params.id
+  const updatedItem = await ItemServices.updateItem(itemId, newData)
+  if (!updatedItem) {
+    console.log('Error when updating item.')
+    return res.redirect('/items')
+  }
+  return res.redirect(`/items/${itemId}`)
+})
 
 /**
  * DELETE: deactivates an existing item in the database (NOT permanent deletion).
- * 
+ *
  * @author Hieu Vo ref Christopher Thacker
  * @since 1.0.0
  */
 router.delete('/:id', async (req, res) => {
-    const itemId = req.params.id;
-    const response = await ItemServices.deleteItem(itemId); // TODO: Currently deletes the item in the DB, but eventually will need to update isActive flag.
-    console.log(response);
-    if (!response) {
-        console.log('Error when deleting item.'); // TODO: Send error message to view.
-    }
-    return res.redirect('/items'); //TODO: Send success message to view.
-});
+  const itemId = req.params.id
+  const response = await ItemServices.deleteItem(itemId) // TODO: Currently deletes the item in the DB, but eventually will need to update isActive flag.
+  console.log(response)
+  if (!response) {
+    console.log('Error when deleting item.') // TODO: Send error message to view.
+  }
+  return res.redirect('/items') //TODO: Send success message to view.
+})
 
-module.exports = router;
+module.exports = router
