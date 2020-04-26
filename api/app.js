0bd26117454ec63d1deaa2ba0b0983668670d2e9
Christopher Thacker <csteven015@gmail.com>
2020-04-25 23:40:06 -0400

    Removed developer test view routers.

--- api/app.js
+++ api/app.js
@@ -25,12 +25,6 @@ const commentsRouter = require('./routes/comments.route')
 const commentReportRouter = require('./routes/comments.reports.route')
 const adminRouter = require('./routes/admin.route')
 
-// Dev Routers
-const devUsersRouter = require('./routes/dev.users.route')
-const devCommentsRouter = require('./routes/dev.comments.route')
-const devPostsRouter = require('./routes/dev.posts.route')
-const devListsRouter = require('./routes/dev.lists.route')
-const devItemsRouter = require('./routes/dev.items.route')
 // Environment Variables Access
 require('dotenv').config()
 
@@ -98,14 +92,6 @@ app.use('/lists', listsRouter)
 app.use('/comments', commentsRouter)
 app.use('/report/comments', commentReportRouter)
 app.use('/admin', adminRouter)
-
-// Dev routes
-app.use('/dev/users/', devUsersRouter)
-app.use('/dev/comments/', devCommentsRouter)
-app.use('/dev/posts/', devPostsRouter)
-app.use('/dev/lists/', devListsRouter)
-app.use('/dev/items/', devItemsRouter)
-
 
 // Connect to Database
 let connection = new DatabaseConnector()
